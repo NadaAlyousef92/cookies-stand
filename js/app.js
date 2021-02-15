1a16bad906627b6cbb72baee9310931d1d8b8cc2
NadaAlyousef92 <nada.alyousef92@gmail.com>
2021-02-15 07:07:40 +0200

    test

---
+++ js/app.js
@@ -0,0 +1,246 @@
+"use strict";
+
+  const seattle= {
+     name:'seattle',
+     min:23,
+     max:65,
+     avg:6.3,
+     noofCustomer:0,
+   time:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
+  }
+
+render:function(){
+
+
+const container = document.getElementById('cookies-stand');
+
+console.log(container);
+const ul = document.createElement('ul');
+container.appendChild(li);
+imgpath:"img/salmon.png";
+
+const h1El=document.createElement('h1');
+articleEl.appendChild(h1El);
+h1El.textContent=this.noofCustomer;
+
+const h1El.textContent=`${this.time}`:${noofCustomer}
+  const imgEl=document.createElement('./img/salmon.png');
+  articleEl.appendChild("img/salmon.png");
+  img.setAttribute('src',this.img/salmon.png);
+  
+
+
+  const ul = document.createElement('ul');
+ // li.textContent = i;
+  ul.appendChild(li);
+  this.time[i];
+  container.appendChild(ul);
+  or(let i = 0 ; i<this.time.length ;i++ ){
+    liEl=document.createElement('li');
+      this.time[i];
+      ulEl.appendChild(liEl);
+      liEl.textContent=this.time[i];
+  }
+
+
+  
+}
+
+
+
+
+
+  const Tokyo= {
+     name:'Tokyo',
+     min:3,
+     max:24,
+     avg:1.2,
+     noofCustomer:0,
+   time:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
+  }
+
+render:function(){
+
+
+const container = document.getElementById('cookies-stand');
+
+console.log(container);
+const ul = document.createElement('ul');
+container.appendChild(li);
+imgpath:"img/salmon.png";
+
+const h2El=document.createElement('h2');
+articleEl.appendChild(h1El);
+h2El.textContent=this.noofCustomer;
+
+const h1El.textContent=`${this.time}`:${noofCustomer}
+  const imgEl=document.createElement('./img/salmon.png');
+  articleEl.appendChild("img/salmon.png");
+  img.setAttribute('src',this.img/salmon.png);
+  
+
+
+  const ul = document.createElement('ul');
+ // li.textContent = i;
+  ul.appendChild(li);
+  this.time[i];
+  container.appendChild(ul);
+  or(let i = 0 ; i<this.time.length ;i++ ){
+    liEl=document.createElement('li');
+      this.time[i];
+      ulEl.appendChild(liEl);
+      liEl.textContent=this.time[i];
+  }
+
+
+  
+}
+
+
+
+
+
+  const Dubai= {
+     name:'Dubai',
+     min:11,
+     max:38,
+     avg:3.7,
+     noofCustomer:0,
+   time:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
+  }
+
+render:function(){
+
+
+const container = document.getElementById('cookies-stand');
+
+console.log(container);
+const ul = document.createElement('ul');
+container.appendChild(li);
+imgpath:"img/salmon.png";
+
+const h1E3=document.createElement('h3');
+articleEl.appendChild(h3El);
+h3El.textContent=this.noofCustomer;
+
+const h1El.textContent=`${this.time}`:${noofCustomer}
+  const imgEl=document.createElement('./img/salmon.png');
+  articleEl.appendChild("img/salmon.png");
+  img.setAttribute('src',this.img/salmon.png);
+  
+
+
+  const ul = document.createElement('ul');
+ // li.textContent = i;
+  ul.appendChild(li);
+  this.time[i];
+  container.appendChild(ul);
+  or(let i = 0 ; i<this.time.length ;i++ ){
+    liEl=document.createElement('li');
+      this.time[i];
+      ulEl.appendChild(liEl);
+      liEl.textContent=this.time[i];
+  }
+
+
+  
+}
+
+
+
+  const paris= {
+     name:'paris',
+     min:20,
+     max:38,
+     avg:2.3,
+     noofCustomer:0,
+   time:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
+  }
+
+render:function(){
+
+
+const container = document.getElementById('cookies-stand');
+
+console.log(container);
+const ul = document.createElement('ul');
+container.appendChild(li);
+imgpath:"img/salmon.png";
+
+const h1E4=document.createElement('h4');
+articleEl.appendChild(h4El);
+h4El.textContent=this.noofCustomer;
+
+const h1El.textContent=`${this.time}`:${noofCustomer}
+  const imgEl=document.createElement('./img/salmon.png');
+  articleEl.appendChild("img/salmon.png");
+  img.setAttribute('src',this.img/salmon.png);
+  
+
+
+  const ul = document.createElement('ul');
+ // li.textContent = i;
+  ul.appendChild(li);
+  this.time[i];
+  container.appendChild(ul);
+  or(let i = 0 ; i<this.time.length ;i++ ){
+    liEl=document.createElement('li');
+      this.time[i];
+      ulEl.appendChild(liEl);
+      liEl.textContent=this.time[i];
+  }
+
+
+  
+}
+
+
+
+
+  const lima= {
+     name:'lima',
+     min:2,
+     max:16,
+     avg:4.6,
+     noofCustomer:0,
+   time:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
+  }
+
+render:function(){
+
+
+const container = document.getElementById('cookies-stand');
+
+console.log(container);
+const ul = document.createElement('ul');
+container.appendChild(li);
+imgpath:"img/salmon.png";
+
+const h5El=document.createElement('h5');
+articleEl.appendChild(h5El);
+h5El.textContent=this.noofCustomer;
+
+const h5El.textContent=`${this.time}`:${noofCustomer}
+  const imgEl=document.createElement('./img/salmon.png');
+  articleEl.appendChild("img/salmon.png");
+  img.setAttribute('src',this.img/salmon.png);
+  
+
+
+  const ul = document.createElement('ul');
+ // li.textContent = i;
+  ul.appendChild(li);
+  this.time[i];
+  container.appendChild(ul);
+  or(let i = 0 ; i<this.time.length ;i++ ){
+    liEl=document.createElement('li');
+      this.time[i];
+      ulEl.appendChild(liEl);
+      liEl.textContent=this.time[i];
+  }
+
+
+  
+}
+
+  
